6fc13a1ed1c69cf555cac074779485b6e33bfe47
hyun <duboo31@gmail.com>
2022-05-12 20:33:40 +0900

    Add function for chat

--- js/main.js
+++ js/main.js
@@ -2,9 +2,28 @@
 const wordInput = document.querySelector(".word-input");
 const wordDisplay = document.querySelector(".word-display");
 const scoreDisplay = document.querySelector(".score");
+const timeDisplay = document.querySelector(".time");
+const button = document.querySelector(".button");
 
 // VARIABLES
+const GAME_TIME = 3;
+
 let score = 0;
+let time = GAME_TIME;
+let isPlaying = false;
+let timeInterval;
+
+//functions
+
+init()
+
+function init() {
+    getWords();
+}
+
+function getWords() {
+    const words = ["a", "b", "c", "d"];
+}
 
 wordInput.addEventListener('input', () => {
     if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
@@ -12,4 +31,28 @@ wordInput.addEventListener('input', () => {
         scoreDisplay.innerText = score;
         wordInput.value = "";
     }
-});
+})
+
+
+function countDown() {
+    time > 0 ? time-- : isPlaying = false;
+    if(!isPlaying) {
+        clearInterval(timeInterval);
+    }
+    timeDisplay.innerText = time;
+}
+
+buttonChange("PLAY");
+
+function run() {
+    isPlaying = true;
+    time = GAME_TIME;
+    timeInterval = setInterval(countDown, 1000);
+}
+
+button.addEventListener("click", run)
+
+function buttonChange(text) {
+    button.innerText = text;
+    text === "PLAY" ? button.classList.remove("loading") : button.classList.add("loading"); 
+}
